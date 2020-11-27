81a36ad1b587771a9f11c6cf97e097a3cd987ce2
Dream-cake <thespider84@gmail.com>
2020-11-27 00:56:35 -0500

    1.0.3 repush

---
+++ README.md
@@ -0,0 +1,3 @@
+# Grace Bot Locales
+
+Why are you even here? Unless you want to help.
---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+files = require("./allowedLangs.json");
+let messages = {website: {}, bot: {}};
+for (i = 0; i < files.length; i++) {
+    file = require(`./langs/${files[i]}`);
+    if (file.website) messages.website[files[i].split('.')[0]] = file.website;
+    if (file.bot) messages.bot[files[i].split('.')[0]] = file.bot;
+}
+
+exports.website = messages.website;
+
+exports.bot = messages.bot;
