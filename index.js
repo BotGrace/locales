0d6af1135d97a6927585beb0e591ca12c2d16ccc
Dream-cake <thespider84@gmail.com>
2020-11-27 00:52:14 -0500

    1.0.3

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 #!/usr/bin/env node
-
 'use strict';
-
-const fs = require('fs');
+files = require("./allowedLangs.json");
 let messages = {website: {}, bot: {}};
-fs.readdir('./langs/', (err, files) => {
-    if (err) throw err;
-    files = files.filter(f => /(^|\/)[A-Za-z0-9_,\s-]+\.json$/i.test(f));
-    for (i = 0; i < files.length; i++) {
-        file = require(`./langs/${files[i]}`);
-        if (file.website) messages.website[files[i].split('.')[0]] = file.website;
-        if (file.bot) messages.bot[files[i].split('.')[0]] = file.bot;
-    }
-});
+for (i = 0; i < files.length; i++) {
+    file = require(`./langs/${files[i]}`);
+    if (file.website) messages.website[files[i].split('.')[0]] = file.website;
+    if (file.bot) messages.bot[files[i].split('.')[0]] = file.bot;
+}
 
 exports.website = messages.website;
 
